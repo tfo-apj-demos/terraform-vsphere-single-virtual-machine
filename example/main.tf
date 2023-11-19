9a27860849106847341f773e835440e55055cec7
Aaron Evans <aarone@outlook.com.au>
2023-11-19 11:46:59 +1100

    default os_type example

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,10 @@
 module "single-virtual-machine" {
   source  = "app.terraform.io/tfo-apj-demos/single-virtual-machine/vsphere"
-  version = "1.1.2"
+  version = "1.1.5"
 
   backup_policy    = "daily"
   environment      = "dev"
+  os_type          = "linux"
   security_profile = "web-server"
   site             = "sydney"
   size             = "medium"
