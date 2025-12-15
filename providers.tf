7ffea3d49cb99c05a5ab940c2e956018bdfc19bc
Aaron Evans <aarone@outlook.com.au>
2025-12-15 16:04:02 +1100

    chore: clean up provider configuration and uncomment necessary sections

--- providers.tf
+++ providers.tf
@@ -1,17 +1,35 @@
 terraform {
   required_providers {
     vsphere = {
-      #source  = "hashicorp/vsphere"
-      source = "vmware/vsphere"
+      source  = "vmware/vsphere"
       version = "~> 2"
     }
     hcp = {
       source  = "hashicorp/hcp"
       version = "~> 0.104"
     }
-    # ad = {
-    #   source  = "hashicorp/ad"
-    #   version = "0.4.4"
-    # }
+    ad = {
+      source  = "hashicorp/ad"
+      version = "~> 0.5"
+    }
+    dns = {
+      source  = "hashicorp/dns"
+      version = "~> 3.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
+}
+
+# vSphere provider configuration
+# Uses environment variables: VSPHERE_USER, VSPHERE_PASSWORD, VSPHERE_SERVER
+provider "vsphere" {
+  allow_unverified_ssl = true
+}
+
+# HCP provider configuration
+provider "hcp" {
+  project_id = "11eb56d6-0f95-3a99-a33c-0242ac110007"
 }
